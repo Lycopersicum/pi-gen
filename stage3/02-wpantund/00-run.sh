9a46dfb02db020d1055f729ac6899d07c5b99b34
Giedrius Medzevičius <giedrius@8devices.com>
2017-06-12 16:42:06 +0300

    Add wpantund service

--- stage3/02-wpantund/00-run.sh
+++ stage3/02-wpantund/00-run.sh
@@ -14,3 +14,8 @@ install -m 755 files/usr/local/libexec/wpantund/ncp-dummy.la  ${ROOTFS_DIR}/usr/
 install -m 755 files/usr/local/libexec/wpantund/ncp-spinel.so ${ROOTFS_DIR}/usr/local/libexec/wpantund/
 install -m 644 files/etc/dbus-1/system.d/wpantund.conf        ${ROOTFS_DIR}/etc/dbus-1/system.d/
 install -m 644 files/etc/wpantund.conf                        ${ROOTFS_DIR}/etc/
+install -m 644 files/etc/systemd/system/wpantund.service      ${ROOTFS_DIR}/etc/systemd/system/
+
+on_chroot << EOF
+  systemctl disable wpantund
+EOF
