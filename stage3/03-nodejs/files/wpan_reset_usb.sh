b307cf5d94e99ed35318e34824f8e7ea8997d89a
Giedrius Medzevičius <giedrius@8devices.com>
2017-10-05 10:13:58 +0300

    [NodeRed] Update 8dev_2100 dongle reset script

--- stage3/03-nodejs/files/wpan_reset_usb.sh
+++ stage3/03-nodejs/files/wpan_reset_usb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USBNAME=STMicroelectronics
+USBNAME="257f:0002"
 LSUSB=$(lsusb | grep --ignore-case $USBNAME)
 FOLD="/dev/bus/usb/"$(echo $LSUSB | cut --delimiter=' ' --fields='2')"/"$(echo $LSUSB | cut --delimiter=' ' --fields='4' | tr --delete ":")
 echo $LSUSB
