e0e75fa109e2af5454565fa79da390d579e354da
Tomas Markevicius <tomas.m@8devices.com>
2018-06-07 17:57:20 +0300

    Implement building restserver from github automatically

--- stage3/05-restserver/00-run.sh
+++ stage3/05-restserver/00-run.sh
@@ -2,20 +2,23 @@
 
 install -d ${ROOTFS_DIR}/usr/local/lib/ ${ROOTFS_DIR}/usr/local/sbin/
 
-install -m 755 files/liborcania.so.1.1 ${ROOTFS_DIR}/usr/local/lib/
-install -m 755 files/libyder.so.2.0    ${ROOTFS_DIR}/usr/local/lib/
-install -m 755 files/libulfius.so.2.1  ${ROOTFS_DIR}/usr/local/lib/
-ln -sf liborcania.so.1.1 ${ROOTFS_DIR}/usr/local/lib/liborcania.so
-ln -sf libyder.so.2.0    ${ROOTFS_DIR}/usr/local/lib/libyder.so
-ln -sf libulfius.so.2.1  ${ROOTFS_DIR}/usr/local/lib/libulfius.so
-
-install -m 755 files/restserver        ${ROOTFS_DIR}/usr/local/sbin/
-install -m 755 files/restserver-ssdp   ${ROOTFS_DIR}/usr/local/sbin/
-
 install -m 644 files/restserver.service ${ROOTFS_DIR}/etc/systemd/system/
-install -m 644 files/restserver-ssdp.service ${ROOTFS_DIR}/etc/systemd/system/
 
 on_chroot << EOF
+  cd
+  git clone https://github.com/babelouest/ulfius.git
+  cd ulfius/
+  git submodule update --init
+  cd lib/orcania
+  make && sudo make install
+  cd ../yder
+  make && sudo make install
+  cd ../..
+  make && sudo make install
+  cd
+  git clone https://github.com/8devices/wakaama.git
+  cd wakaama
+  mkdir build && cd build && cmake ../examples/rest-server && make
+  install -m 755 restserver /usr/local/sbin/
   systemctl enable restserver
-  systemctl enable restserver-ssdp
 EOF
